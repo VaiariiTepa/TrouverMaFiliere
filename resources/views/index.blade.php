e811df0c27289ff443ff1371ec68539ad38800b9
VaiariiTEPA <tepavaiarii@gmail.com>
2021-03-26 00:53:22 -1000

    link script adjust for laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/template.css') }}" rel="stylesheet">
+        <link href="css/app.css'" rel="stylesheet">
+        <link href="css/template.css'" rel="stylesheet">
         <!-- <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet"> -->
         <!-- Styles -->
         <style>
